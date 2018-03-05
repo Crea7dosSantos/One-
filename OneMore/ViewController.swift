6247e9cd1ced68d44855f8227993daef80802ebe
Cria7dosSantos <yusaku@ikedayuusaku-no-macbook-air.local>
2018-03-05 13:42:12 +0900

    Initial Commit

---
+++ OneMore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneMore
+//
+//  Created by 池田優作 on 2018/03/05.
+//  Copyright © 2018年 yusaku.ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
