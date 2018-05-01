7c00b573601160ac171a22741428759e7ad08198
Cria7dosSantos <yusaku@ikedayuusaku-no-macbook-air.local>
2018-05-01 15:19:45 +0900

    Realmファイルを中心にcommitできていなかった部分を実行。

---
+++ OneMore/Realm/Back.swift
@@ -0,0 +1,36 @@
+//
+//  Back.swift
+//  OneMore
+//
+//  Created by 池田優作 on 2018/03/22.
+//  Copyright © 2018年 yusaku.ikeda. All rights reserved.
+//
+
+import RealmSwift
+
+class Back: Object {
+  // 管理用 ID。プライマリーキー
+  @objc dynamic var id = 0
+  
+  // 投稿ID
+  @objc dynamic var postID = ""
+  
+  // UserName
+  @objc dynamic var userName = ""
+  
+  // ImageString
+  @objc dynamic var imageString = ""
+  
+  // caption
+  @objc dynamic var caption = ""
+  
+  // date
+  @objc dynamic var time = ""
+  
+  // 公開、非公開タグ
+  @objc dynamic var privateSwitch: Bool = true
+  
+  override static func primaryKey() -> String? {
+    return "id"
+  }
+}
---
+++ OneMore/Realm/Noon.swift
@@ -0,0 +1,37 @@
+//
+//  Noon.swift
+//  OneMore
+//
+//  Created by 池田優作 on 2018/04/09.
+//  Copyright © 2018年 yusaku.ikeda. All rights reserved.
+//
+
+import RealmSwift
+
+class Noon: Object {
+  // 管理用ID。プライマリーキー
+  @objc dynamic var id = 0
+  
+  // 投稿ID
+  @objc dynamic var postID = ""
+  
+  // UserName
+  @objc dynamic var userName = ""
+  
+  // ImageString
+  @objc dynamic var imageString = ""
+  
+  // caption
+  @objc dynamic var caption = ""
+  
+  // date
+  @objc dynamic var time = ""
+  
+  // 公開、非公開タグ
+  @objc dynamic var privateSwitch: Bool = true
+  
+  override static func primaryKey() -> String? {
+    return "id"
+  }
+}
+
